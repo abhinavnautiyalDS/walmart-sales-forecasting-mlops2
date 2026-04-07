b6b7170a35b10497f92829bcc71a94d969e3c8c2
Abhinav Nautiyal <164336356+abhinavnautiyalDS@users.noreply.github.com>
2026-04-07 23:17:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 const API_URL = "https://walmart-sales-forecasting-mlops2.onrender.com/predict"
 
-const upload = document.getElementById("csvUpload")
-
 let dataset = []
 
+const upload = document.getElementById("csvUpload")
+const runBtn = document.getElementById("runPrediction")
 
 upload.addEventListener("change", async function(){
 
 const file = upload.files[0]
 
 if(!file){
+
 alert("Please upload CSV")
 return
+
 }
 
 const text = await file.text()
 
 dataset = parseCSV(text)
 
+alert("File loaded. Now click Run Prediction.")
+
+})
+
+
+runBtn.addEventListener("click", function(){
+
+if(dataset.length === 0){
+
+alert("Upload CSV first")
+return
+
+}
+
 sendForPrediction(dataset)
 
 })
+
 
 
 function parseCSV(text){
@@ -98,6 +115,24 @@ line:{color:"#0071dc"}
 }])
 
 }
+
+
+document.getElementById("downloadTemplate").addEventListener("click", function(){
+
+const csv = `Store,Date,Dept,IsHoliday,Temperature,Fuel_Price,MarkDown1,MarkDown2,MarkDown3,MarkDown4,MarkDown5,CPI,Unemployment,Type,Size
+1,2012-01-06,1,0,45,3.2,0,0,0,0,0,220,7.2,1,151315`
+
+const blob = new Blob([csv], { type: 'text/csv' })
+
+const link = document.createElement("a")
+
+link.href = URL.createObjectURL(blob)
+
+link.download = "walmart_template.csv"
+
+link.click()
+
+})
 
 
 document.querySelectorAll(".tab").forEach(tab=>{
